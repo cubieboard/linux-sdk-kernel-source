bbef50d018dd3995162f03db5fd0171c7edda7d6
Sam <sam@cubietech.com>
2015-06-04 16:50:51 +0800

    fix tp ft5x06 request irq failed
    the irq don't supprot IRQF_TRIGGER_FALLING in A20 (in my test: don't support in a10 too)

--- drivers/input/touchscreen/ft5x_ts.c
+++ drivers/input/touchscreen/ft5x_ts.c
@@ -1762,7 +1762,7 @@ ft5x_ts_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		pr_info("%s:ctp_ops.set_irq_mode err.\n", __func__);
 		goto exit_set_irq_mode;
 	}
-	err = request_irq(SW_INT_IRQNO_PIO, ft5x_ts_interrupt, IRQF_TRIGGER_FALLING | IRQF_SHARED, "ft5x_ts", ft5x_ts);
+	err = request_irq(SW_INT_IRQNO_PIO, ft5x_ts_interrupt,IRQF_SHARED, "ft5x_ts", ft5x_ts);
 
 	if (err < 0) {
 		dev_err(&client->dev, "ft5x_ts_probe: request irq failed\n");
